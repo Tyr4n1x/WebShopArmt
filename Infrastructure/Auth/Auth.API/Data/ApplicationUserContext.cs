71c270e4b0434d0dbdec5a0512080da880b85065
BenDeSchampheleire <99195628+BenDeSchampheleire@users.noreply.github.com>
2025-06-13 07:16:06 +0200

    Add the DbContext for the newly defined model ApplicationUser: ApplicationUserContext

---
+++ Infrastructure/Auth/Auth.API/Data/ApplicationUserContext.cs
@@ -0,0 +1,13 @@
+﻿using Auth.API.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Auth.API.Data
+{
+    public class ApplicationUserContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationUserContext(DbContextOptions<ApplicationUserContext> options) : base(options)
+        {
+        }
+    }
+}
