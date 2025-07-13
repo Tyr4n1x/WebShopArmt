85315e77dd4918fe35f97f74beb2cfd093b06abc
BenDeSchampheleire <99195628+BenDeSchampheleire@users.noreply.github.com>
2025-07-13 20:01:13 +0200

    Created a DTO for updating a product: UpdateProductRequest

---
+++ src/Services/Catalog.API/DTOs/UpdateProductRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Catalog.API.DTOs
+{
+    public class UpdateProductRequest : CreateProductRequest
+    {
+        [Required]
+        public required Guid Id { get; set; }
+    }
+}
