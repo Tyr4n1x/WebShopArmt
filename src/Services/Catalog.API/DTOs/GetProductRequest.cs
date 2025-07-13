b96a1e5c2152b3e933162b393dbc0948f84f41cd
BenDeSchampheleire <99195628+BenDeSchampheleire@users.noreply.github.com>
2025-07-13 19:59:54 +0200

    Created a DTO for getting a product: GetProductRequest

---
+++ src/Services/Catalog.API/DTOs/GetProductRequest.cs
@@ -0,0 +1,27 @@
+﻿namespace Catalog.API.DTOs
+{
+    public class GetProductRequest
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public string Category { get; set; } = string.Empty;
+
+        public string? Brand { get; set; }
+
+        public decimal Price { get; set; } = 0.0m;
+
+        public decimal? DiscountedPrice { get; set; }
+
+        public bool OnSale { get; set; }
+
+        public int? Stock { get; set; }
+
+        public string? PictureUri { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
