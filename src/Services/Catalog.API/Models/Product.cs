eac7c69a6686529d587bef3402d4c441a7540b09
BenDeSchampheleire <99195628+BenDeSchampheleire@users.noreply.github.com>
2025-07-13 16:40:51 +0200

    Created a model for the Products.

---
+++ src/Services/Catalog.API/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿namespace Catalog.API.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public string Category { get; set; } = string.Empty;
+
+        public string? Brand { get; set; }
+
+        public decimal Price { get; set; } = 0.01m;
+
+        public decimal? DiscountedPrice { get; set; }
+
+        public bool OnSale => DiscountedPrice.HasValue && DiscountedPrice.Value < Price;
+
+        public int? Stock { get; set; }
+
+        public string? PictureUri { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
