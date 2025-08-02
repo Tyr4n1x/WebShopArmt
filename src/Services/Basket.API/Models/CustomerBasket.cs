5ed4ef5b386eddd8d898ab2f11507d1072713090
BenDeSchampheleire <99195628+BenDeSchampheleire@users.noreply.github.com>
2025-08-02 10:35:50 +0200

    Created a model CustomerBasket, representing the customer's basket.

---
+++ src/Services/Basket.API/Models/CustomerBasket.cs
@@ -0,0 +1,8 @@
+﻿namespace Basket.API.Models
+{
+    public class CustomerBasket(string customerId)
+    {
+        public string CustomerId { get; set; } = customerId;
+        public List<BasketItem> Items { get; set; } = [];
+    }
+}
