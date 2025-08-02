94d0bfac4002d166c9359fa4f3e475d5e310139d
BenDeSchampheleire <99195628+BenDeSchampheleire@users.noreply.github.com>
2025-08-02 10:56:24 +0200

    Created an interface IBasketService defining the contract for operations on CustomerBaskets.

---
+++ src/Services/Basket.API/Services/IBasketService.cs
@@ -0,0 +1,11 @@
+﻿using Basket.API.Models;
+
+namespace Basket.API.Services
+{
+    public interface IBasketService
+    {
+        Task<CustomerBasket?> GetBasketAsync(string customerId);
+        Task<CustomerBasket> UpdateBasketAsync(CustomerBasket basket);
+        Task<bool> DeleteBasketAsync(string customerId);
+    }
+}
