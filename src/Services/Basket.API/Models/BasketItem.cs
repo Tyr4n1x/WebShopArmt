785a07b26976ad76055f7000c95f754e26f6286e
BenDeSchampheleire <99195628+BenDeSchampheleire@users.noreply.github.com>
2025-08-02 10:35:14 +0200

    Created a model BasketItem, representing a particular item in the customer's basket.

---
+++ src/Services/Basket.API/Models/BasketItem.cs
@@ -0,0 +1,11 @@
+﻿namespace Basket.API.Models
+{
+    public class BasketItem
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal UnitPrice { get; set; } = 0.01m;
+        public int Quantity { get; set; } = 0;
+        public string? PictureUri { get; set; }
+    }
+}
