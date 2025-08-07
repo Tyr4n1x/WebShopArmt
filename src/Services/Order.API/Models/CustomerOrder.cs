64ae9a507ab5fdf9dd71bb1eee84e2db517ee5db
BenDeSchampheleire <99195628+BenDeSchampheleire@users.noreply.github.com>
2025-08-07 16:09:58 +0200

    Created a model CustomerOrder to represent an order placed by a customer, consisting of multiple OrderItems.

---
+++ src/Services/Order.API/Models/CustomerOrder.cs
@@ -0,0 +1,11 @@
+﻿namespace Order.API.Models
+{
+    public class CustomerOrder(string customerId)
+    {
+        public Guid Id { get; set; }
+        public string CustomerId { get; set; } = customerId;
+        public List<OrderItem> Items { get; set; } = [];
+        public decimal Total => Items.Sum(i => i.UnitPrice * i.Quantity);
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ src/Services/Order.API/Models/OrderItem.cs
@@ -0,0 +1,11 @@
+﻿namespace Order.API.Models
+{
+    public class OrderItem
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal UnitPrice { get; set; } = 0.01m;
+        public int Quantity { get; set; } = 0;
+        public string? PictureUri { get; set; }
+    }
+}
