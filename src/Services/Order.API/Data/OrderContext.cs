4f436f9fa86e46a31d8dd6712a8063eacfe5ad8e
BenDeSchampheleire <99195628+BenDeSchampheleire@users.noreply.github.com>
2025-08-07 16:30:35 +0200

    Created a DatabaseContext for the CustomerOrder Model and added this context to the services in Program.cs.

---
+++ src/Services/Order.API/Data/OrderContext.cs
@@ -0,0 +1,12 @@
+﻿using Order.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Order.API.Data
+{
+    public class OrderContext : DbContext
+    {
+        public OrderContext(DbContextOptions<OrderContext> options) : base(options) { }
+
+        public DbSet<CustomerOrder> Orders => Set<CustomerOrder>();
+    }
+}
--- src/Services/Order.API/Program.cs
+++ src/Services/Order.API/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using Order.API.Data;
 
 namespace Order.API
 {
@@ -7,7 +9,9 @@ namespace Order.API
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            // Configures the DbContextFactory for the OrderContext with a InMemory database (for now)
+            builder.Services.AddDbContext<OrderContext>(opt =>
+                opt.UseInMemoryDatabase("OrderDatabase"));
 
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
