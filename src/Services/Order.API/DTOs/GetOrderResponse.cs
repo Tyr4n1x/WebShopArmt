87e3862f0a0039b693168deda6543ca0d736ddc0
BenDeSchampheleire <99195628+BenDeSchampheleire@users.noreply.github.com>
2025-08-08 16:30:34 +0200

    Created a DTO for getting an order: GetOrderResponse

---
+++ src/Services/Order.API/DTOs/GetOrderResponse.cs
@@ -0,0 +1,13 @@
+﻿using Order.API.Models;
+
+namespace Order.API.DTOs
+{
+    public class GetOrderResponse
+    {
+        public Guid Id { get; set; }
+        public string CustomerId { get; set; } = string.Empty;
+        public List<OrderItem> Items { get; set; } = [];
+        public decimal Total { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
