b9a08a71aaf83a05389923270fa696c85958755b
BenDeSchampheleire <99195628+BenDeSchampheleire@users.noreply.github.com>
2025-08-26 21:40:31 +0200

    Created a PaymentService that interacts with the Stripe framework to create PaymentIntents and construct Webhooks.

---
+++ src/Services/Order.API/Services/PaymentService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Options;
+using Stripe;
+
+namespace Order.API.Services
+{
+    public class PaymentService
+    {
+        private readonly string _secretKey;
+
+        public PaymentService(IOptions<StripeSettings> stripeSettings)
+        {
+            _secretKey = stripeSettings.Value.SecretKey;
+            StripeConfiguration.ApiKey = _secretKey;
+        }
+
+        public async Task<PaymentIntent> CreatePaymentIntentAsync(decimal amount, string currency = "eur")
+        {
+            var options = new PaymentIntentCreateOptions
+            {
+                Amount = (long)(amount * 100), // Stripe works in cents
+                Currency = currency,
+                PaymentMethodTypes = ["bancontact", "card", "ideal", "paypal", "sepa_debit", "sofort"]
+            };
+
+            var service = new PaymentIntentService();
+            return await service.CreateAsync(options);
+        }
+
+        public Event ConstructWebhookEvent(string json, string stripeSignature, string webhookSecret)
+        {
+            return EventUtility.ConstructEvent(json, stripeSignature, webhookSecret);
+        }
+    }
+}
