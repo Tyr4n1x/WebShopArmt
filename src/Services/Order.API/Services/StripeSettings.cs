5ebbb086988530017d4c83a2b32dc714e754a1fe
BenDeSchampheleire <99195628+BenDeSchampheleire@users.noreply.github.com>
2025-08-26 08:44:33 +0200

    Added the package Stripe to the current project. Defined a class StripeSettings to contain the configuration parameters of Stripe.

---
+++ src/Services/Order.API/Services/StripeSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Order.API.Services
+{
+    public class StripeSettings
+    {
+        public string SecretKey { get; set; } = string.Empty;
+        public string PublicKey { get; set; } = string.Empty;
+        public string WebhookSecret { get; set; } = string.Empty;
+    }
+}
