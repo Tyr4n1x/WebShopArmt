653a92957e28970117e22a0a4d71298537a3ca83
BenDeSchampheleire <99195628+BenDeSchampheleire@users.noreply.github.com>
2025-09-21 12:14:04 +0200

    Created an interface IIntegrationEventHandler for defining Handlers of IntegrationEvents.

---
+++ BuildingBlocks/EventBus/Interfaces/IIntegrationEventHandler.cs
@@ -0,0 +1,10 @@
+﻿using EventBus.Events;
+
+namespace EventBus.Interfaces
+{
+    public interface IIntegrationEventHandler<in TIntegrationEvent>
+        where TIntegrationEvent : IntegrationEvent
+    {
+        Task Handle(TIntegrationEvent @event);
+    }
+}
