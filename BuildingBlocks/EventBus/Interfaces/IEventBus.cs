ec8a99afaf261ee9e75068a3b690d2621edbaadb
BenDeSchampheleire <99195628+BenDeSchampheleire@users.noreply.github.com>
2025-09-21 12:15:25 +0200

    Defined the interface IEventBus for an event bus abstraction. It specifies methods to publish, subscribe, and unsubscribe to integration events, without direct ties to a specific service.

---
+++ BuildingBlocks/EventBus/Interfaces/IEventBus.cs
@@ -0,0 +1,15 @@
+﻿using EventBus.Events;
+
+namespace EventBus.Interfaces
+{
+    public interface IEventBus
+    {
+        void Publish(IntegrationEvent @event);
+        void Subscribe<T, TH>()
+            where T : IntegrationEvent
+            where TH : IIntegrationEventHandler<T>;
+        void Unsubscribe<T, TH>()
+            where T : IntegrationEvent
+            where TH : IIntegrationEventHandler<T>;
+    }
+}
