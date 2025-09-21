e6d69fa817bbf91eda06c211cbefe1c3cc170c1c
BenDeSchampheleire <99195628+BenDeSchampheleire@users.noreply.github.com>
2025-09-21 14:58:46 +0200

    Defined the Interface IRabbitMQPersistentConnection, which is a wrapper for RabbitMQ connections.

---
+++ BuildingBlocks/EventBus/EventBusRabbitMQ/IRabbitMQPersistentConnection.cs
@@ -0,0 +1,11 @@
+﻿using RabbitMQ.Client;
+
+namespace EventBus.EventBusRabbitMQ
+{
+    public interface IRabbitMQPersistentConnection : IDisposable
+    {
+        bool IsConnected { get; }
+        Task<bool> TryConnectAsync();
+        ValueTask<IChannel> CreateChannelAsync();
+    }
+}
