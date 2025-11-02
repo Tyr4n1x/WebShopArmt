f9ecf6515780d0ce8955632471caed7b7aaca4a8
BenDeSchampheleire <99195628+BenDeSchampheleire@users.noreply.github.com>
2025-11-02 10:58:36 +0100

    Created an IntegrationEvent PaymentSucceededIntegrationEvent that is called when a succesful payment is received.

---
+++ BuildingBlocks/EventBus/Events/PaymentSucceededIntegrationEvent.cs
@@ -0,0 +1,14 @@
+﻿namespace EventBus.Events
+{
+    public class PaymentSucceededIntegrationEvent : IntegrationEvent
+    {
+        public Guid OrderId { get; set; }
+        public decimal Amount { get; set; }
+
+        public PaymentSucceededIntegrationEvent(Guid orderId, decimal amount)
+        {
+            OrderId = orderId;
+            Amount = amount;
+        }
+    }
+}
