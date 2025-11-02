0d3110c015efdd3e64391e23b791060ead3a3e6a
BenDeSchampheleire <99195628+BenDeSchampheleire@users.noreply.github.com>
2025-11-02 11:05:07 +0100

    Defined a class DependencyInjection inside the EventBus, containing the method AddEventBus. This is a shared EventBus registration helper.

---
+++ BuildingBlocks/EventBus/DependencyInjection.cs
@@ -0,0 +1,36 @@
+﻿using EventBus.EventBusRabbitMQ;
+using EventBus.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+
+namespace EventBus
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddEventBus(this IServiceCollection services, string hostName = "localhost")
+        {
+            services.AddSingleton<IRabbitMQPersistentConnection>(sp =>
+            {
+                var logger = sp.GetRequiredService<ILogger<DefaultRabbitMQPersistentConnection>>();
+                var factory = new ConnectionFactory
+                {
+                    HostName = hostName
+                };
+
+                return new DefaultRabbitMQPersistentConnection(factory, logger);
+            });
+
+            services.AddSingleton<IEventBus, EventBus.EventBusRabbitMQ.EventBusRabbitMQ>(sp =>
+            {
+                var connection = sp.GetRequiredService<IRabbitMQPersistentConnection>();
+                var logger = sp.GetRequiredService<ILogger<EventBus.EventBusRabbitMQ.EventBusRabbitMQ>>();
+                var provider = sp;
+
+                return new EventBus.EventBusRabbitMQ.EventBusRabbitMQ(connection, provider, logger);
+            });
+
+            return services;
+        }
+    }
+}
