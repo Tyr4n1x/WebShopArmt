cdfd99c8d372fdd9660fbc403dedd6230d06470a
BenDeSchampheleire <99195628+BenDeSchampheleire@users.noreply.github.com>
2025-11-02 10:59:55 +0100

    Defined an EventHandler PaymentSucceededIntegrationEventHandler inside the Order.API to handle an PaymentSucceededIntegrationEvent.

---
+++ src/Services/Order.API/IntegrationEvents/Handlers/PaymentSucceededIntegrationEventHandler.cs
@@ -0,0 +1,33 @@
+﻿using EventBus.Events;
+using EventBus.Interfaces;
+using Order.API.Data;
+
+namespace Order.API.IntegrationEvents.Handlers
+{
+    public class PaymentSucceededIntegrationEventHandler : IIntegrationEventHandler<PaymentSucceededIntegrationEvent>
+    {
+        private readonly OrderContext _context;
+        private readonly ILogger<PaymentSucceededIntegrationEventHandler> _logger;
+
+        public PaymentSucceededIntegrationEventHandler(OrderContext context, ILogger<PaymentSucceededIntegrationEventHandler> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        public async Task HandleAsync(PaymentSucceededIntegrationEvent @event)
+        {
+            var order = await _context.Orders.FindAsync(@event.OrderId);
+            if (order is null)
+            {
+                _logger.LogWarning("Order not found for ID {OrderId}", @event.OrderId);
+                return;
+            }
+
+            order.PaymentStatus = "Succeeded";
+            await _context.SaveChangesAsync();
+
+            _logger.LogInformation("Updated order {OrderId} payment status to 'Succeeded'.", @event.OrderId);
+        }
+    }
+}
